adbe4780ea5b6a93013ffba43b6466ea85aa9044
Charles Lenk <lenkc@Charless-MacBook-Air.local>
2025-08-16 16:47:45 -0700

    Improved export config logic

--- export-script/export_config.py
+++ export-script/export_config.py
@@ -9,9 +9,11 @@ from pathlib import PurePath
 from threading import Lock
 
 conf_file_name = 'export.conf'
-lock = Lock()
+default_export_file_name = 'export map.scad'
 
 openSCADLocationName = 'openSCADLocation'
+projectRootName = 'projectRoot'
+exportFilePathName = 'exportFilePath'
 stlOutputDirectoryName = 'stlOutputDirectory'
 
 def is_openscad_location_valid(location):
@@ -19,6 +21,19 @@ def is_openscad_location_valid(location):
 
 def is_path_writable(directory):
     return os.access(os.path.dirname(directory), os.W_OK)
+
+def is_valid_path(path):
+    return os.path.isdir(os.path.dirname(path))
+
+def get_file_path(file_name):
+    project_root = _get_project_root()
+    print('Searching for {} in {}'.format(file_name, project_root))
+    for root, dirs, files in os.walk(project_root):
+        if file_name in files:
+            return str(os.path.join(root, file_name))
+
+def is_scad_file(file_name):
+    return file_name.lower().endswith('.scad') and get_file_path(file_name) is not None
 
 def reprompt(validation_func, input_name):
     user_input = input('Enter {} or "q" to exit: '.format(input_name))
@@ -30,12 +45,20 @@ def reprompt(validation_func, input_name):
     else:
         return user_input
 
-@cache
 def _get_project_root():
-    project_root = PurePath(__file__).parents[2]
-    if not os.path.isdir(project_root):
+    process = Popen(['git', 'rev-parse', '--show-toplevel'], cwd=str(PurePath(__file__).parents[1]), stdout=PIPE, stderr=PIPE)
+    project_root, err = process.communicate()
+    project_root = str(project_root, encoding='UTF-8').strip()
+    if not is_valid_path(project_root):
         project_root = reprompt(os.path.isdir, 'project root folder')
-    return str(project_root)
+    print('Project root: {}'.format(project_root))
+    return project_root
+
+def _get_export_file_path():
+    export_file_name = default_export_file_name
+    if not is_scad_file(export_file_name):
+        export_file_name = reprompt(is_scad_file, 'export file name (.scad)')
+    return get_file_path(export_file_name)
 
 def _get_openscad_location():
     system = platform.system()
@@ -47,7 +70,7 @@ def _get_openscad_location():
         else:
             location = 'C:\\Program Files\\OpenSCAD\\openscad.exe'
     elif (system == 'Darwin'):
-        location = '/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD.app'
+        location = '/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD'
     elif (system == 'Linux'):
         location = 'openscad'
     if not is_openscad_location_valid(location):
@@ -65,7 +88,6 @@ def _get_stl_output_directory():
         directory = reprompt(is_path_writable, 'STL output directory')
     return directory
 
-@cache
 def _get_manifold_support(openscad_location):
     if openscad_location:
         process = Popen([openscad_location, '-h'], stdout=PIPE, stderr=PIPE)
@@ -74,43 +96,31 @@ def _get_manifold_support(openscad_location):
     else:
         return False
 
-def validate_config(config):
-    validated_config = config.copy()
-    if not is_openscad_location_valid(config.get(openSCADLocationName, '')):
-        validated_config[openSCADLocationName] = _get_openscad_location()
-    if not is_path_writable(config.get(stlOutputDirectoryName, '')):
-        validated_config[stlOutputDirectoryName] = _get_stl_output_directory()
-    return validated_config
+class ExportConfig:
+    def _validate(config):
+        validated_config = config.copy()
+        if not is_openscad_location_valid(config.get(openSCADLocationName, '')):
+            validated_config[openSCADLocationName] = _get_openscad_location()
+        if not is_valid_path(config.get(projectRootName, '')):
+            validated_config[projectRootName] = _get_project_root()
+        if not is_valid_path(config.get(exportFilePathName, '')):
+            validated_config[exportFilePathName] = _get_export_file_path()
+        if not is_path_writable(config.get(stlOutputDirectoryName, '')):
+            validated_config[stlOutputDirectoryName] = _get_stl_output_directory()
+        return validated_config
 
-@cache
-def get_config():
-    conf_file = os.path.join(_get_project_root(), conf_file_name)
-    config = {}
-    if os.path.isfile(conf_file):
-        with open(conf_file, 'r') as file:
-            config = json.load(file)
-    validated_config = validate_config(config)
-    if validated_config != config:
-        with open(conf_file, 'w') as file:
-            json.dump(validated_config, file, indent=2)
-    return validated_config
-
-@cache
-def get_project_root():
-    with lock:
-        return _get_project_root()
-
-@cache
-def get_openscad_location():
-    with lock:
-        return get_config()[openSCADLocationName]
-
-@cache
-def get_stl_output_directory():
-    with lock:
-        return get_config()[stlOutputDirectoryName]
-
-@cache
-def get_manifold_support():
-    with lock:
-        return _get_manifold_support(get_openscad_location())
+    def __init__(self):
+        conf_file = os.path.join(PurePath(__file__).parents[0], conf_file_name)
+        config = {}
+        if os.path.isfile(conf_file):
+            with open(conf_file, 'r') as file:
+                config = json.load(file)
+        validated_config = self._validate(config)
+        if validated_config != config:
+            with open(conf_file, 'w') as file:
+                json.dump(validated_config, file, indent=2)
+        self.openSCADLocation = validated_config[openSCADLocationName]
+        self.manifoldSupport = _get_manifold_support(self.openSCADLocation)
+        self.projectRoot = validated_config[projectRootName]
+        self.exportFilePath = validated_config[exportFilePathName]
+        self.stlOutputDirectory = validated_config[stlOutputDirectoryName]
--- export-script/stl_export.py
+++ export-script/stl_export.py
@@ -2,31 +2,8 @@ import os
 import shutil
 from concurrent.futures import ThreadPoolExecutor
 from subprocess import Popen, PIPE
-from export_config import get_openscad_location, get_stl_output_directory, get_manifold_support, get_project_root
+from export_config import ExportConfig
 from numbers import Number
-
-part_map = {
-    'cupiter': {
-        'frame': {
-            'folder_test': {
-                'arm_upper': {
-                    'part': 'arm_upper',
-                    'quantity': 2,
-                    'size': 20
-                },
-                'arm_lower': {
-                    'part': 'arm_lower',
-                    'size': 20
-                }
-            },
-            'test_test': {
-                'part': 'leg_lower',
-                'quantity': 2,
-                'size': 20
-            }
-        }
-    }
-}
 
 def is_part_definition(dictionary):
     return all(not isinstance(value, dict) for value in dictionary.values())
@@ -43,15 +20,14 @@ def flatten_to_folders_and_parts(parts, current_path = ''):
             folders_and_parts.update(flatten_to_folders_and_parts(value, current_path + '/' + key))
     return folders_and_parts
 
-def generate_part(output_directory, folder, part):
+def generate_part(export_config, output_directory, folder, part):
     part_file_name = part[0] + '.stl'
-    print(part_file_name)
     os.makedirs(output_directory, exist_ok=True)
 
     args = [
-        get_openscad_location(),
+        export_config.openSCADLocation,
         '-o' + output_directory + part_file_name,
-        get_project_root() + '/src/scad/print map.scad'
+        export_config.exportFilePath
     ]
 
     for arg, value in part[1].items():
@@ -60,13 +36,11 @@ def generate_part(output_directory, folder, part):
         elif isinstance(value, str):
             args.append('-D' + arg + '="' + value + '"')
 
-    if get_manifold_support():
+    if export_config.manifoldSupport:
         args.append('--enable=manifold')
 
     process = Popen(args, stdout=PIPE, stderr=PIPE)
     _, err = process.communicate()
-
-    print(args)
 
     count = part[1].get('quantity', 1)
     output = ""
@@ -81,15 +55,14 @@ def generate_part(output_directory, folder, part):
     return output
 
 def print_parts(part_map):
+    export_config = ExportConfig()
     with ThreadPoolExecutor(max_workers = os.cpu_count()) as executor:
         print('Starting STL generation')
         futures = []
         for folder, part_group in flatten_to_folders_and_parts(part_map).items():
-            output_directory = get_stl_output_directory() + folder + '/'
+            output_directory = export_config.stlOutputDirectory + folder + '/'
             for part in part_group.items():
-                futures.append(executor.submit(generate_part, output_directory, folder, part))
+                futures.append(executor.submit(generate_part, export_config, output_directory, folder, part))
         for future in futures:
             print(future.result())
         print('Done!')
-
-print_parts(part_map)
